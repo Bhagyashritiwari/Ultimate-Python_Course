9ae4a447a32ddc22091dc632b3a971ae08e0e6e0
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-17 23:26:21 +0530

    Create 04_Tuple_methos.py

---
+++ Chapter 4/04_Tuple_methos.py
@@ -0,0 +1,9 @@
+a= (1,56,343, "Bani", "Bani", False, 0.344) # we don't change triple
+print(type(a))
+print(a)
+
+num = a.count("Bani")# find number of count tuple value
+print(num)
+
+i = a.index("Bani")#in index find Bani name index
+print(i)
