4c6565d0fd2e298b6f6ffca3860a54c819966344
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-17 23:26:12 +0530

    Create 02_list_method.py

---
+++ Chapter 4/02_list_method.py
@@ -0,0 +1,31 @@
+friends = ["Aman", "Rishabh", "Kamal", "Bani"] # Append method for last new add string
+friends.append("Karthik")
+print(friends)
+
+#• l1.sort(): updates the list to 
+l1 = [1,89,8,15,21,2]# sort method
+l1.sort()
+print(l1)
+
+#• l1.reverse(): updates the list to 
+l2 = [34,56,78,89,46]
+l2.reverse()
+print(l2)
+
+#• l1.insert(3,8): This will add 8 at 3 index
+l3 = [45,56,56,78,67]
+l3.insert(4, 9999)# i insert 9999 string in 4 index
+print(l3)
+
+#• l1.pop(2): Will delete element at index 2 and return its value.
+
+l4 =[45,566,355,54475,5456]
+print(l4.pop(4))# in list index 4 pop out 
+print(l4)
+
+
+#l1.remove(21): Will remove 21 from the list. 
+
+l5= [89,778,9878,21,43345]
+l5.remove(21)
+print(l5)
