b6f74c7029aac42eff015fd25fa0891758e26470
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-19 15:24:42 +0530

    Create Keyvalue.py

---
+++ Practice/Keyvalue.py
@@ -0,0 +1,12 @@
+#then here is my question: I want you to convert this given dictionary into arrays and then iterate that array to print all key-value pairs:
+
+student_grades = {
+    "Alice": 90,
+    "Bob": 85,
+    "Charlie": 92,
+    "Diana": 88
+}
+
+array = [student_grades]
+
+print(array)
