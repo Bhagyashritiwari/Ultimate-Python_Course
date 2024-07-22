b1879ec663116bc2b367af021d46a834c2aa2fcc
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-22 16:51:18 +0530

    Create 06_Problem.py

---
+++ Chapter 5 PS/06_Problem.py
@@ -0,0 +1,21 @@
+# Create an empty dictionary. Allow 4 friends to enter their favorite language as
+# value and use key as their names. Assume that the names are unique.
+
+Dictionary = {}
+Friends = input("Enter friend name:")
+Langauge = input ("Enter lang name:")
+Dictionary.update({Friends : Langauge})
+
+Friends = input("Enter friend name:")
+Langauge = input ("Enter lang name:")
+Dictionary.update({Friends : Langauge})
+
+Friends = input("Enter friend name:")
+Langauge = input ("Enter lang name:")
+Dictionary.update({Friends : Langauge}) 
+
+Friends = input("Enter friend name:")
+Langauge = input ("Enter lang name:")
+
+Dictionary.update({Friends : Langauge})
+print(Dictionary)
