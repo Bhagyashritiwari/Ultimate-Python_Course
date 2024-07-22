a61ed4b039d16070f89c2be11a51ec1e37aaa9f7
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-22 15:55:18 +0530

    Create Problem.py

---
+++ Chapter 5 PS/Problem.py
@@ -0,0 +1,9 @@
+# Write a program to create a dictionary of Hindi words with values as their English
+# translation. Provide user with an option to look it up!
+
+Meaning = {"kursi":"Chair",
+         "madad" : "help",
+         "Billi" : "Cat"}
+
+word = input("Enter the word you want to meaning of:")
+print(Meaning[word])
