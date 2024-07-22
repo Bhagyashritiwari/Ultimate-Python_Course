24793ddf980d34e1bf9214bfbf72cafae5060171
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-22 16:51:24 +0530

    Create 07_Problem.py

---
+++ Chapter 5 PS/07_Problem.py
@@ -0,0 +1,23 @@
+# . If the names of 2 friends are same; what will happen to the program in problem
+# 6?
+
+# because name is the key of this program so updtae latest one and print that name 
+
+Dictionary = {}
+Friends = input("Enter friend name:")
+Langauge = input ("Enter lang name:")
+Dictionary.update({Friends : Langauge})
+
+Friends = input("Enter friend name:")
+Langauge = input ("Enter lang name:")
+Dictionary.update({Friends : Langauge})
+
+Friends = input("Enter friend name:")
+Langauge = input ("Enter lang name:")
+Dictionary.update({Friends : Langauge}) 
+
+Friends = input("Enter friend name:")
+Langauge = input ("Enter lang name:")
+
+Dictionary.update({Friends : Langauge})
+print(Dictionary)
