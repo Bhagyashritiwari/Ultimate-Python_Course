ebff56320525630bef199174bc8a8bb4936ea4de
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-22 14:48:22 +0530

    Create Unique.py

---
+++ Practice/Unique.py
@@ -0,0 +1,16 @@
+s = set()
+Fruits = input("Enter fruits name:")
+s.add(Fruits)
+Fruits = input("Enter fruits name:")
+s.add(Fruits)
+Fruits = input("Enter fruits name:")
+s.add(Fruits)
+Fruits = input("Enter fruits name:")
+s.add(Fruits)
+Fruits = input("Enter fruits name:")
+s.add(Fruits)
+Fruits = input("Enter fruits name:")
+s.add(Fruits)
+
+
+print(s)
