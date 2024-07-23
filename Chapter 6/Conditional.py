c51d2250d67fd420d5d77f234bd9093c9326b845
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-23 10:39:31 +0530

    Create Conditional.py

---
+++ Chapter 6/Conditional.py
@@ -0,0 +1,12 @@
+a = int(input("Enter your age:"))
+
+#If, else Statment
+
+if(a>=18):
+    print("You are above the age of consent")
+
+else:
+    print("You are below the age of consent")
+
+    
+print("End the program")
