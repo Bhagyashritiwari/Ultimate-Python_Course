00b1d2ee1bc8bdc3c8cfb49f01394511866ed2cb
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-23 14:37:24 +0530

    Create Multiple_If_Statement.py

---
+++ Chapter 6/Multiple_If_Statement.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your age:"))
+
+#If statement 1
+if(a%2==0):
+    print("a is even")
+    
+#If statement 2
+if(a>=18):
+    print("you are above the age")
+
+else:
+    print("You are below the age")    
+
