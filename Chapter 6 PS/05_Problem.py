ae1bef90049ffea787213bed4d57351a5f0917ea
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-23 17:36:35 +0530

    Create 05_Problem.py

---
+++ Chapter 6 PS/05_Problem.py
@@ -0,0 +1,11 @@
+# Write a program which finds out whether a given name is present in a list or not
+
+l = ["Aman", "Bani", "Karthik", "Kamal"]
+
+name = input("enter name: ")
+
+if(name in l):
+    print("List name avaible in this")
+
+else:
+    print("listname not avaible in this ")    
