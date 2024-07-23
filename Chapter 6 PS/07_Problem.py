352be86b792aca438c61065f7485e1044f3bbc9a
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-23 17:36:44 +0530

    Create 07_Problem.py

---
+++ Chapter 6 PS/07_Problem.py
@@ -0,0 +1,9 @@
+# Write a program to find out whether a given post is talking about “Bani” or not
+
+post = input("Enter the post:")
+
+if("Bani".lower in post.lower):
+    print("This post is all about Bani")
+
+else:
+    print("This post is not about the Bani")    
