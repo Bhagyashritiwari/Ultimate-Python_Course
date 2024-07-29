b779bcdae3d5873a9e2104119b9fa70ba9323853
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-29 22:18:25 +0530

    Create List_using_while.py

---
+++ Chapter 7/List_using_while.py
@@ -0,0 +1,7 @@
+#Quick Quiz: Write a program to print the content of a list using while loops
+
+l =["Bani", 0.99, "Kamal", 0.89, 89] # for list use length 
+i =0 
+while(i<len(l)):
+    print(l[i])
+    i+=1
