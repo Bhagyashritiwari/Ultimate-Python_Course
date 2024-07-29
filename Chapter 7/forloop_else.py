3b5bc3463ce07188b56200ccce1978c467f0da43
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-29 22:49:02 +0530

    Create forloop_else.py

---
+++ Chapter 7/forloop_else.py
@@ -0,0 +1,10 @@
+# FOR LOOP WITH ELSE
+# An optional else can be used with a for loop if the code is to be executed when the
+# loops exhausts.
+
+name = [12,12,34,55,56]
+for i in name:
+    print(i)
+
+else:
+    print("Name not here")    
