5ee8ad6d028cd371e48e092d595fc36044e482b6
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-07-29 22:18:18 +0530

    Create For_loop.py

---
+++ Chapter 7/For_loop.py
@@ -0,0 +1,20 @@
+for i in range(4):
+    print(i)
+
+    print("--------")
+
+for n in range (0, 100, 4):
+    print(n)
+
+print("-----------------")
+
+#for loop for tuple
+l=[76,456,346,456]
+for i in l:
+    print(i)
+
+print("------------")   
+#For loop for string
+s ="Bani","Kamal","Aman"
+for i in s :
+    print(i)
