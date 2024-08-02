cb1bb127e112f89a277425ed90e330700de1862e
Bhagyashri <bhagyashritiwarialm@gmail.com>
2024-08-02 10:17:04 +0530

    Create 5_Problem.py

---
+++ Chapter 7 PS/5_Problem.py
@@ -0,0 +1,21 @@
+# Write a program to find the sum of first n natural numbers using while loop
+
+
+n = int(input("Enter the number: "))
+i = 1
+sum = 0
+while(i<=n):
+    sum += i
+    i+=1
+
+print(sum)
+
+# n = int(input("Enter the nmber:"))
+
+# i = 1
+# sum = 0
+# while(i<=n):
+#     sum += i    
+#     i+=1
+    
+# print(sum)
