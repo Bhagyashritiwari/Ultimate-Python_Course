a48385107646722fec0189c6dcfd1758277330b4
Bhagyashri <103115092+Bhagyashritiwari@users.noreply.github.com>
2025-01-30 17:39:13 +0530

    Update Keyvalue.py

--- Practice/Keyvalue.py
+++ Practice/Keyvalue.py
@@ -1,7 +1,7 @@
 #then here is my question: I want you to convert this given dictionary into arrays and then iterate that array to print all key-value pairs:
 
 student_grades = {
-    "Alice": 90,
+    "Alice": 901,
     "Bob": 85,
     "Charlie": 92,
     "Diana": 88
